a7ec727265b4118316b18c0270435247a5820a88
Дмитрий <_shkirando_@MacBook-Pro-Dmitrij.local>
2024-06-03 13:29:35 +0300

    Добавлены тестовые данные

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #2. Conference Programm\n";
     cout << "Author: Dmitry Shkirando\n";
+    cout << "Group: 23AI1d\n";
     return 0;
 }
