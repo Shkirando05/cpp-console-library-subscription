43a4fe831b167b695d143d777718e95831a2048c
Shkirando05 <dima.shkirando2005@gmail.com>
2024-06-03 12:57:28 +0300

    Добавлен вывод общей информации о задании

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #2. Conference Programm\n";
+    cout << "Author: Dmitry Shkirando\n";
     return 0;
 }
